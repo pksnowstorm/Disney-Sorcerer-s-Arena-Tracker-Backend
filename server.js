de3028069db1dfc88cc529f0bc8ffc687f7bbe30
pksnowstorm <enguyen21@gmail.com>
2023-05-18 09:50:44 -0500

    add basic app use, mongo connection and test home page

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const { PORT = 4000, MONGODB_URL } = process.env;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+mongoose.connection
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extend: true}));
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
