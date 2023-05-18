4b9e3025a5c1f90d082a2e66c64cb6acbf8d8201
pksnowstorm <enguyen21@gmail.com>
2023-05-18 10:33:33 -0500

    comment out unnecessary stuff that prevents testing

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 require("dotenv").config();
-const { PORT = 4000, MONGODB_URL } = process.env;
+const { PORT = 4000 } = process.env;
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const cors = require("cors");
-const morgan = require("morgan");
+//const mongoose = require("mongoose");
+//const cors = require("cors");
+//const morgan = require("morgan");
 
-mongoose.connect(MONGODB_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-});
+// mongoose.connect(MONGODB_URL, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+// });
 
-mongoose.connection
-    .on("open", () => console.log("You are connected to mongoose"))
-    .on("close", () => console.log("You are disconnected from mongoose"))
-    .on("error", (error) => console.log(error));
+// mongoose.connection
+//     .on("open", () => console.log("You are connected to mongoose"))
+//     .on("close", () => console.log("You are disconnected from mongoose"))
+//     .on("error", (error) => console.log(error));
 
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({extend: true}));
+// app.use(cors());
+// app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({extend: true}));
 
 app.get("/", (req, res) => {
     res.send("hello world");
 });
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
