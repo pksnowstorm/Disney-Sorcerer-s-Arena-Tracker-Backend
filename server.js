b04318451383ee3e304c4450b08802ccf5bf2545
pksnowstorm <enguyen21@gmail.com>
2023-05-18 10:35:41 -0500

    Update to test mongoose connection

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
-const { PORT = 4000 } = process.env;
+const { PORT = 4000, MONGODB_URL } = process.env;
 const express = require("express");
 const app = express();
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 //const cors = require("cors");
 //const morgan = require("morgan");
 
-// mongoose.connect(MONGODB_URL, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-// });
+ mongoose.connect(MONGODB_URL, {
+     useUnifiedTopology: true,
+     useNewUrlParser: true,
+ });
 
-// mongoose.connection
-//     .on("open", () => console.log("You are connected to mongoose"))
-//     .on("close", () => console.log("You are disconnected from mongoose"))
-//     .on("error", (error) => console.log(error));
+ mongoose.connection
+     .on("open", () => console.log("You are connected to mongoose"))
+     .on("close", () => console.log("You are disconnected from mongoose"))
+     .on("error", (error) => console.log(error));
 
 // app.use(cors());
 // app.use(morgan("dev"));
