0b1d97e57d54e65d340481f1d39995070320ccb3
pksnowstorm <enguyen21@gmail.com>
2023-05-19 09:33:22 -0500

    Add models

--- server.js
+++ server.js
@@ -16,6 +16,55 @@ const mongoose = require("mongoose");
      .on("close", () => console.log("You are disconnected from mongoose"))
      .on("error", (error) => console.log(error));
 
+const UserSchema = new mongoose.Schema({
+    account: String,
+    username: String,
+    password: String
+})
+
+const AccountSchema = new mongoose.Schema ({
+    name: String,
+    characters: Number,
+    spells: Number,
+    club: String,
+    events: String
+})
+
+const CharacterSchema = new mongoose.Schema ({
+    name: String,
+    tags: String,
+    special1: String,
+    special2: String,
+    passive: String,
+    starLevel: Number,
+    gearTier: Number,
+    farm: String,
+    stone: String
+})
+
+const SpellSchema = new mongoose.Schema ({
+    name: String,
+    description: String,
+    farm: String
+})
+
+const ClubSchema = new mongoose.Schema({
+    name: String,
+    members: Number,
+    clubExpeditionTier: String,
+    forbiddenDepthsRaidTier: String,
+    siegeOfOlympusTier: String,
+    alliance: String,
+    contactInfo: String
+});
+
+const EventSchema = new mongoose.Schema({
+    name: String,
+    type: String,
+    requirements: String,
+    qualified: Boolean
+});
+
 // app.use(cors());
 // app.use(morgan("dev"));
 // app.use(express.json());
